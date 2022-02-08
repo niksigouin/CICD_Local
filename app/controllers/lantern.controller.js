bfb65dcdd8d35a54ece1b7276780de28536960db
Navak Server <navakserver@gmail.com>
2022-02-08 15:26:24 -0500

    added initial files

---
+++ app/controllers/lantern.controller.js
@@ -0,0 +1,153 @@
+const db = require("../models");
+const Lantern = db.lanterns;
+const utils = require("../utils");
+
+// Create and Save a new user
+exports.create = async (req, res) => {
+    // Validate request
+    if (!req.body) {
+        res.status(400).send({ message: "Content can not be empty!" });
+        return;
+    }
+
+    try {
+        const element = await Lantern.findOne({ macAddress: req.body.macAddress });
+        // console.log(test);
+        if (element != null) {
+            console.log(`Lantern [ID: ${element.id} | IP: ${req.body.ipAddress} | MAC: ${req.body.macAddress}] already exists`)
+            res.send(element);
+        } else {
+            const lantern = new Lantern({
+                hostName: req.body.hostName,
+                macAddress: req.body.macAddress,
+                ipAddress: req.body.ipAddress,
+            });
+
+            await lantern.save(lantern);
+            res.send(lantern);
+            console.log(`CREATED Lantern [ID: ${req.body.id} | IP: ${req.body.ipAddress}  | MAC: ${req.body.macAddress}]`)
+        }
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({
+            message: error
+        });
+    }
+};
+
+// Retrieve all Lanterns from the database.
+exports.findAll = async (req, res) => {
+    const title = req.query.title;
+    var condition = title ? { title: { $regex: new RegExp(title), $options: "i" } } : {};
+    try {
+        const data = await Lantern.find(condition);
+        res.send(data);
+    } catch (error) {
+        res.status(500).send({
+            message: error
+        });
+    }
+};
+
+exports.findActive = async (req, res) => {
+    var query = {status : true};
+    try {
+        const allActive = await Lantern.find(query);
+        console.log("ALL ACTIVE:")
+        console.log(allActive);
+        res.send(allActive);
+        
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({
+            message: error
+        });
+    }
+};
+
+  // Update a User by the id in the request
+  exports.updateStatus = async (req, res) => {
+    // console.log('req', req.body);
+    if (!req.body) {
+      return res.status(400).send({
+        message: "Data to update can not be empty!"
+      });
+    }
+    try {
+        var query = {macAddress: req.body.macAddress};
+        var newValues = {status: true};
+        const target = await Lantern.findOneAndUpdate(query, newValues);
+        console.log(`Lantern [ID: ${target.id} | IP: ${target.ipAddress} | MAC: ${target.macAddress}] is Online!`);
+        res.send(`Lantern ${target['ipAddress']} is Online!`);
+        utils.pulseOSC();
+    } catch (error) {
+      console.log('error', error);
+      res.status(500).send({
+        message: error
+      });
+    }
+  };
+
+  // Find a single User with an id
+//   exports.findOne = async (req, res) => {
+//     const id = req.params.id;
+//     try {
+//       const user = await Lantern.findById(id);
+//       client.send('/base_data,', JSON.stringify(user), (err) =>{
+//         if(err) console.log(err);
+//       })
+//       res.send(user);
+//     } catch (error) {
+//       res.status(500).send({
+//         message: error
+//       });
+//     }
+//   };
+
+//   // Update a User by the id in the request
+//   exports.update = async (req, res) => {
+//     console.log('req', req.body);
+//     if (!req.body) {
+//       return res.status(400).send({
+//         message: "Data to update can not be empty!"
+//       });
+//     }
+//     const id = req.params.id;
+//     try {
+//       await User.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+//         res.send(`User ${id} updated successful!`);
+//     } catch (error) {
+//       console.log('error', error);
+//       res.status(500).send({
+//         message: error
+//       });
+//     }
+//   };
+
+//   // Delete a User with the specified id in the request
+//   exports.delete = async (req, res) => {
+//     const id = req.params.id;
+//     try {
+//       await User.findByIdAndRemove(id)
+//         res.send(`User ${id} deleted successful!`);
+//     } catch (error) {
+//       console.log('error', error);
+//       res.status(500).send({
+//         message: error
+//       });
+//     }
+//   };
+
+//   // Delete all Users from the database.
+//   exports.deleteAll = async (req, res) => {
+//     try {
+//       await User.deleteMany({})
+//         res.send(`Database deleted successful!`);
+//     } catch (error) {
+//       console.log('error', error);
+//       res.status(500).send({
+//         message: error
+//       });
+//     }
+//   };
---
+++ index.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const cors = require("cors");
+var netping  = require("net-ping");
+var ping = require("ping");
+const utils = require("./app/utils");
+
+var pingcfg = { 
+  timeout: 2,
+  extra: ['-i', '2']
+}
+
+const app = express();
+
+
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+const db = require("./app/models");
+const lanternRoutes = require("./app/routes/lantern.routes");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to wd application." });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8181;
+require("./app/routes/lantern.routes")(app);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+  GetAllActive();
+  const pingInterval = setInterval(() => {
+    GetAllActive();
+  }, 5000);
+
+});
+
+
+async function GetAllActive() {
+  var query = {status : true};
+    try {
+        const allActive = await db.lanterns.find(query);
+        allActive.forEach(lantern => {
+          ping.sys.probe(lantern['ipAddress'], (status) => {
+            if(!status){
+              UpdateState(lantern['ipAddress']);
+            }
+          }, pingcfg);
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function UpdateState(ipAddress) {
+  var query = {ipAddress: ipAddress};
+  var newValues = {status: false};
+  const target = await db.lanterns.findOneAndUpdate(query, newValues);
+  console.log(`Lantern [ID: ${target.id} | IP: ${target.ipAddress} | MAC: ${target.macAddress}] is Offline!`);
+  utils.pulseOSC();
+}
+
